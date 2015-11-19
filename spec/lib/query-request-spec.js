018bbe6a6f6b80e1ee6afe15abf9c65e5bc18f7c
Tom Cass <casst01@outlook.com>
2015-11-19 20:04:28 +0000

    Pass opts to xml builder

--- spec/lib/query-request-spec.js
+++ spec/lib/query-request-spec.js
@@ -3,15 +3,28 @@ var QueryRequest = require('../../lib/query-request');
 
 describe('Query Request', function() {
 
+  var query = 'collection("test-collection")//message/body';
+  var namespaces = { 'xmlns': 'http://exist.sourceforge.net/NS/exist' };
+
   describe('Building request XML', function() {
     it('should build an eXist-db request XML structure for the given query', function() {
-      var query = 'collection("test-collection")//message/body';
       var requestBody = new QueryRequest(query).build();
-      expect(parseXml(requestBody)).toBe([
-          '<query xmlns="http://exist.sourceforge.net/NS/exist">',
-          '<text>collection("test-collection")//message/body</text>',
-          '</query>'
-        ].join(''));
+      expect(parseXml(requestBody)).toContain([
+        '<query xmlns="http://exist.sourceforge.net/NS/exist">',
+        '<text>collection("test-collection")//message/body</text>',
+        '</query>'
+      ].join(''));
+    });
+
+    describe('Options', function() {
+      describe('When user specifies range options', function() {
+        it('should add range attributes', function() {
+          var requestBody = new QueryRequest(query, { start: 2, max: 10 }).build();
+          var xmlDoc = libxml.parseXmlString(requestBody);
+          expect(xmlDoc.get('/xmlns:query/@start', namespaces).value()).toBe('2');
+          expect(xmlDoc.get('/xmlns:query/@max', namespaces).value()).toBe('10');
+        });
+      });
     });
   });
 
