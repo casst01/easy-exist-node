f1f29229d2f548f9ed7834fcc9d375cbdaaf9368
Tom Cass <casst01@outlook.com>
2015-12-12 16:43:55 +0000

    Added executeStoredQuery function. Syntactic sugar. Simply calls get()

--- spec/lib/db-spec.js
+++ spec/lib/db-spec.js
@@ -277,36 +277,66 @@ describe('DB', function() {
 
     });
 
-    describe('#storeQuery', function() {
+    describe('Stored Queries', function() {
 
       var query = {
         body: 'let $var := 1\nreturn <var>{$var}</var>',
         uri: '/my-collection/stored-queries/test.xqy'
       };
 
-      it('should store the given query', function(done) {
-        db.storeQuery(query.uri, query.body)
-          .then(function() {
-            return db.exists(query.uri);
-          })
-          .then(function(queryExists) {
-            expect(queryExists).toBe(true);
-          })
-          .catch(function(err) {
-            console.log(err.response.statusMessage);
-          })
-          .then(done);
+      describe('#storeQuery', function() {
+
+        it('should store the given query', function(done) {
+          db.storeQuery(query.uri, query.body)
+            .then(function() {
+              return db.exists(query.uri);
+            })
+            .then(function(queryExists) {
+              expect(queryExists).toBe(true);
+            })
+            .catch(function(err) {
+              console.log(err.response.statusMessage);
+            })
+            .then(done);
+        });
+
+        describe('when uri does not contain a preceding slash', function(){
+          it('should raise an error', function(done) {
+            var uriWithoutPrecedingSlash = 'my-document';
+            db.storeQuery(uriWithoutPrecedingSlash, query.body)
+              .catch(function(err) {
+                expect(err.message).toBe('ArgumentError: \"uri\" must contain preceding \'/\'');
+                done();
+              });
+          });
+        });
+
       });
 
-      describe('when uri does not contain a preceding slash', function(){
-        it('should raise an error', function(done) {
-          var uriWithoutPrecedingSlash = 'my-document';
-          db.storeQuery(uriWithoutPrecedingSlash, query.body)
-            .catch(function(err) {
-              expect(err.message).toBe('ArgumentError: \"uri\" must contain preceding \'/\'');
-              done();
-            });
+      describe('#executeStoredQuery', function() {
+
+        it('should return the result of running the stored query', function(done) {
+          db.storeQuery(query.uri, query.body)
+            .then(function() {
+              return db.executeStoredQuery(query.uri);
+            })
+            .then(function(queryResults) {
+              expect(queryResults).toBe('<var>1</var>');
+            })
+            .then(done);
         });
+
+        describe('when uri does not contain a preceding slash', function(){
+          it('should raise an error', function(done) {
+            var uriWithoutPrecedingSlash = 'my-document';
+            db.executeStoredQuery(uriWithoutPrecedingSlash)
+              .catch(function(err) {
+                expect(err.message).toBe('ArgumentError: \"uri\" must contain preceding \'/\'');
+                done();
+              });
+          });
+        });
+
       });
 
     });
